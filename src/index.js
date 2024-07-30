ca872e1baad7d0a4d0095ff8a74fb3956047b1e6
SuriyaPrakash7 <suriya20037@gmail.com>
2024-07-30 23:44:35 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import{BrowserRouter as Router} from "react-router-dom"
+
+
+import "./index.css";
+
+import {App} from "./containers";
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <Router>
+            <App>
+
+            </App>
+        </Router>
+    </React.StrictMode>
+
+);
---
+++ src/pages/Authentication.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Logo} from "../assets"
+
+
+
+const Authentication = () => {
+  return (
+    <div className="auth-section">
+
+    </div>
+  )
+};
+
+
+export default Authentication
